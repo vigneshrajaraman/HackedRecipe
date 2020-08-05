c5f1dfa54093179cec1adccbb5c7b47322360512
vigneshrajaraman <53133803+vigneshrajaraman@users.noreply.github.com>
2020-08-05 22:56:07 +0530

    finesh

--- src/app/recipe-service.service.ts
+++ src/app/recipe-service.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
+@Injectable()
 export class RecipeService {
 
   constructor(private http: HttpClient ) { }
